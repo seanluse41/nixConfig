192fd721d49ec4380fbcf85d5cea3ce9369a9c46
seanluse <sean@kintone.com>
2026-05-07 17:21:07 +0900

    use mtp

--- modules/features/dev/ai.nix
+++ modules/features/dev/ai.nix
@@ -17,9 +17,8 @@
           llamaFlake.rocm
         else if hostName == "aiServer" then
           llamaFlake.rocm.overrideAttrs (old: {
-            cmakeFlags = (builtins.filter
-              (f: builtins.match ".*CMAKE_HIP_ARCHITECTURES.*" f == null)
-              old.cmakeFlags)
+            cmakeFlags =
+              (builtins.filter (f: builtins.match ".*CMAKE_HIP_ARCHITECTURES.*" f == null) old.cmakeFlags)
               ++ [ "-DCMAKE_HIP_ARCHITECTURES:STRING=gfx1031;gfx1200" ];
           })
         else
@@ -38,7 +37,7 @@
           After = [ "network.target" ];
         };
         Service = {
-          ExecStart = "${llama}/bin/llama-server -hf am17an/Qwen3.6-27B-MTP-GGUF:Q6_K --spec-type mtp --spec-draft-n-max 3 --jinja -c 32768 --host 0.0.0.0 --port 8033 -np 1 --min-p 0.0 --webui-mcp-proxy --no-mmproj --no-mmap -t 8 -tb 8 -ctk q8_0 -ctv q8_0 -ngl 99";
+          ExecStart = "${llama}/bin/llama-server -hf froggeric/Qwen3.6-27B-MTP-GGUF:Q6_K --spec-type mtp --spec-draft-n-max 3 --jinja -c 32768 --host 0.0.0.0 --port 8033 -np 1 --min-p 0.0 --webui-mcp-proxy --no-mmproj --no-mmap -t 8 -tb 8 -ctk q8_0 -ctv q8_0 -ngl 99";
           Restart = "on-failure";
         };
         Install = {
