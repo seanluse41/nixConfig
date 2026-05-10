187e6ad9b9bfbe6401e509ebc7334b2970282f89
seanluse41 <seanluse41@gmail.com>
2026-05-10 21:17:36 +0900

    hermes

--- modules/features/services/hermes.nix
+++ modules/features/services/hermes.nix
@@ -2,6 +2,19 @@
   flake.nixosModules.hermesAgent = { config, ... }: {
     imports = [ inputs.hermes-agent.nixosModules.default ];
 
+    sops.secrets."SLACK_BOT_TOKEN" = {};
+    sops.secrets."SLACK_APP_TOKEN" = {};
+    sops.secrets."SLACK_ALLOWED_USERS" = {};
+
+    sops.templates."hermes-env" = {
+      owner = "hermes";
+      content = ''
+        SLACK_BOT_TOKEN=${config.sops.placeholder."SLACK_BOT_TOKEN"}
+        SLACK_APP_TOKEN=${config.sops.placeholder."SLACK_APP_TOKEN"}
+        SLACK_ALLOWED_USERS=${config.sops.placeholder."SLACK_ALLOWED_USERS"}
+      '';
+    };
+
     services.hermes-agent = {
       enable = true;
       settings = {
@@ -9,21 +22,8 @@
         model.base_url = "http://192.168.50.49:8033/v1";
         model.api_key = "local";
       };
-      environmentFiles = [ config.sops.secrets."hermes-env".path ];
+      environmentFiles = [ config.sops.templates."hermes-env".path ];
       addToSystemPackages = true;
     };
-
-    sops.secrets."hermes-env" = {
-      owner = "hermes";
-      template = ''
-        SLACK_BOT_TOKEN=${config.sops.placeholder."SLACK_BOT_TOKEN"}
-        SLACK_APP_TOKEN=${config.sops.placeholder."SLACK_APP_TOKEN"}
-        SLACK_ALLOWED_USERS=${config.sops.placeholder."SLACK_ALLOWED_USERS"}
-      '';
-    };
-
-    sops.secrets."SLACK_BOT_TOKEN" = {};
-    sops.secrets."SLACK_APP_TOKEN" = {};
-    sops.secrets."SLACK_ALLOWED_USERS" = {};
   };
 }
