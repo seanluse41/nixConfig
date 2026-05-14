613c26134636aee4a2989f961db16defd4f341e2
Sean Luse <41235727+seanluse41@users.noreply.github.com>
2026-05-14 13:56:26 +0900

    revert default.nix

--- modules/hosts/macbook/default.nix
+++ modules/hosts/macbook/default.nix
@@ -1,63 +1,44 @@
 { self, inputs, ... }:
 {
-  flake.darwinConfigurations."seanluse" = inputs.nix-darwin.lib.darwinSystem {
-    system = "aarch64-darwin";
-    modules = with self.nixosModules; [
-      inputs.home-manager.darwinModules.home-manager
-      inputs.sops-nix.darwinModules.sops
-      inputs.mac-app-util.darwinModules.default
+  flake.homeConfigurations."seanluse" = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = import inputs.nixpkgs {
+      system = "aarch64-darwin";
+      config.allowUnfree = true;
+    };
+    extraSpecialArgs = {
+      inherit self inputs;
+      nixpkgs-hugo = inputs.nixpkgs-hugo;
+      nixpkgs-sass = inputs.nixpkgs-sass;
+      hostName = "macbook";
+    };
+    modules = with self.homeModules; [
+      inputs.mac-app-util.homeManagerModules.default
+      inputs.sops-nix.homeManagerModules.sops
+      aerospace
+      starship
+      zsh
+      kintone
+      vscode
+      communications
+      hugo
+      devPackages
+      utils
+      aws
+      ai
       {
-        nixpkgs.config.allowUnfree = true;
-
+        home.username = "seanluse";
+        home.homeDirectory = "/Users/sean";
+        home.stateVersion = "25.05";
+        programs.home-manager.enable = true;
         sops = {
           defaultSopsFile = "${self}/secrets/secrets.yaml";
-          age.keyFile = "/Users/sean/Library/Application Support/sops/age/keys.txt";
-          secrets.GITHUB_TOKEN = { };
+          age.keyFile = "/Users/sean/.config/sops/age/keys.txt";
         };
-
-        users.users.seanluse.home = "/Users/sean";
-        system.primaryUser = "seanluse";
-        system.stateVersion = 6;
-        system.activationScripts.postActivation.text = ''
-  mkdir -p /Users/sean/.config/nix
-  echo "access-tokens = github.com=$(cat /run/secrets/GITHUB_TOKEN)" > /Users/sean/.config/nix/nix.conf
-'';
-
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          backupFileExtension = "backup";
-          extraSpecialArgs = {
-            inherit self inputs;
-            nixpkgs-hugo = inputs.nixpkgs-hugo;
-            nixpkgs-sass = inputs.nixpkgs-sass;
-            hostName = "macbook";
-          };
-          users.seanluse = { ... }: {
-            imports = with self.homeModules; [
-              aerospace
-              starship
-              zsh
-              kintone
-              vscode
-              communications
-              hugo
-              devPackages
-              utils
-              aws
-              ai
-            ];
-            home.username = "seanluse";
-            home.homeDirectory = "/Users/sean";
-            home.stateVersion = "25.05";
-            programs.home-manager.enable = true;
-            home.packages = with inputs.nixpkgs.legacyPackages.aarch64-darwin; [
-              age
-              sops
-              nerd-fonts.jetbrains-mono
-            ];
-          };
-        };
+        home.packages = with inputs.nixpkgs.legacyPackages.aarch64-darwin; [
+          age
+          sops
+          nerd-fonts.jetbrains-mono
+        ];
       }
     ];
   };
