f6276345ca344e13dddd414e5fa07527f818d646
seanluse41 <seanluse41@gmail.com>
2026-05-17 21:31:01 +0900

    build llama

--- modules/features/dev/ai.nix
+++ modules/features/dev/ai.nix
@@ -11,6 +11,25 @@ in
       ...
     }:
     let
+      webuiAssets = {
+        "index.html" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/index.html?download=true";
+          sha256 = "sha256-PqVtrGlFbswvMa2E2ekSFVrjfyR6JI1/KBB6Ita8SvM=";
+        };
+        "bundle.js" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/bundle.js?download=true";
+          sha256 = "sha256-+alYqhMf1mckplyr4FGj/4JoIx/6z/KhEzhw8yHBBHo=";
+        };
+        "bundle.css" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/bundle.css?download=true";
+          sha256 = "sha256-TrHUVuoyZTUcAbWqPXcG0hXR5M78L7cN5Y99cD02wXs=";
+        };
+        "loading.html" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/loading.html?download=true";
+          sha256 = "sha256-JQAFfjmrgVGNFrKPXQGfYQe1irtHsqMNM4YtnntwPNw=";
+        };
+      };
+
       llama =
         if hostName == "macbook" then
           pkgs.llama-cpp
@@ -19,6 +38,14 @@ in
             cmakeFlags =
               (builtins.filter (f: builtins.match ".*CMAKE_HIP_ARCHITECTURES.*" f == null) old.cmakeFlags)
               ++ [ "-DCMAKE_HIP_ARCHITECTURES:STRING=gfx1031;gfx1200" ];
+
+            postConfigure = (old.postConfigure or "") + ''
+              mkdir -p tools/ui/dist
+              cp ${webuiAssets."index.html"} tools/ui/dist/index.html
+              cp ${webuiAssets."bundle.js"} tools/ui/dist/bundle.js
+              cp ${webuiAssets."bundle.css"} tools/ui/dist/bundle.css
+              cp ${webuiAssets."loading.html"} tools/ui/dist/loading.html
+            '';
           });
     in
     {
