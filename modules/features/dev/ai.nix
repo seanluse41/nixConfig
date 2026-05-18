ba53ca74ba929732cc55fa337b5f48ff039c644e
seanluse41 <seanluse41@gmail.com>
2026-05-18 14:53:53 +0900

    use vulkan

--- modules/features/dev/ai.nix
+++ modules/features/dev/ai.nix
@@ -11,11 +11,38 @@ in
       ...
     }:
     let
+      webuiAssets = {
+        "index.html" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/index.html?download=true";
+          sha256 = "sha256-PqVtrGlFbswvMa2E2ekSFVrjfyR6JI1/KBB6Ita8SvM=";
+        };
+        "bundle.js" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/bundle.js?download=true";
+          sha256 = "sha256-+alYqhMf1mckplyr4FGj/4JoIx/6z/KhEzhw8yHBBHo=";
+        };
+        "bundle.css" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/bundle.css?download=true";
+          sha256 = "sha256-TrHUVuoyZTUcAbWqPXcG0hXR5M78L7cN5Y99cD02wXs=";
+        };
+        "loading.html" = pkgs.fetchurl {
+          url = "https://huggingface.co/buckets/ggml-org/llama-ui/resolve/latest/loading.html?download=true";
+          sha256 = "sha256-JQAFfjmrgVGNFrKPXQGfYQe1irtHsqMNM4YtnntwPNw=";
+        };
+      };
+
       llama =
         if hostName == "macbook" then
           pkgs.llama-cpp
         else
-          inputs.llama-cpp.packages.${pkgs.system}.vulkan;
+          inputs.llama-cpp.packages.${pkgs.system}.vulkan.overrideAttrs (old: {
+            postConfigure = (old.postConfigure or "") + ''
+              mkdir -p tools/ui/dist
+              cp ${webuiAssets."index.html"} tools/ui/dist/index.html
+              cp ${webuiAssets."bundle.js"} tools/ui/dist/bundle.js
+              cp ${webuiAssets."bundle.css"} tools/ui/dist/bundle.css
+              cp ${webuiAssets."loading.html"} tools/ui/dist/loading.html
+            '';
+          });
     in
     {
       home.packages = with pkgs; [
